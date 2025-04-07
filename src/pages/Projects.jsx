5e5d5946880b223442acce6dec62999b0a1908df
Lucas Alba <66455475+lucas-alba@users.noreply.github.com>
2025-04-07 11:31:41 -0400

    Text

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,7 +9,7 @@ const projects = [
     link: '/TradingSim',
   },
   {
-    title: 'Solar Forecasting - Live Demo',
+    title: 'Solar Prediction - Live Demo',
     description: 'Daily output predictor using ML & weather APIs.',
     link: 'https://apex.oracle.com/pls/apex/r/carce/carce/solar-prediction-gbr-hosting-page?session=12883315525910',
     external: true,
